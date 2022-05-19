acf293d94a8d0595f08fc69b135763b6075c795c
Paul Jeong <paulj3@illinois.edu>
2022-05-18 23:36:38 -0400

    up to portfolio done

---
+++ src/components/about/About.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import './about.css'
+import ME from '../../assets/me-about.jpg'
+import {FaAward} from 'react-icons/fa'
+import {FiUsers} from 'react-icons/fi'
+import {BsFolderPlus} from 'react-icons/bs'
+
+const About = () => {
+  return (
+    <section id='about'>
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="About Image" />
+          </div>
+        </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+            <article className='about__card'>
+              <FaAward className='about__icon'/>
+              <h5>Experience</h5>
+              <small>3+ Years Working</small>
+            </article>
+
+            <article className='about__card'>
+              <FiUsers className='about__icon'/>
+              <h5>Clients</h5>
+              <small>200+ Worldwide</small>
+            </article>
+
+            <article className='about__card'>
+              <BsFolderPlus className='about__icon'/>
+              <h5>Projects</h5>
+              <small>80+ Completed Projects</small>
+            </article>
+          </div>
+
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+            Itaque, sapiente repellat ex natus quam veritatis? 
+            A delectus, blanditiis excepturi id aliquid quas. 
+            Dolore inventore sequi cum delectus tempore quia quos!
+          </p>
+
+          <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default About
